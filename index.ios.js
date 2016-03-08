31067eabb91c94755edf2d64744c6875884faf0e
comountainclimber <mlasky46@gmail.com>
2016-03-08 13:16:15 -0700

    ...

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,7 @@ import React, {
   AppRegistry,
   Component,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View
@@ -16,22 +17,71 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class AwesomeProject extends Component {
-  render() {
-    var movie= MOCKED_MOVIES_DATA[0];
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+    render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return(
+      <ListView
+        dataSource = {this.state.dataSource}
+        renderRow = {this.renderMovie}
+        style = {styles.listView}
+      />
+    )
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Image 
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
           source={{uri: movie.posters.thumbnail}}
           style={styles.thumbnail}
-        >
-        </Image>
-      <View style={styles.rightContainer}>
-        <Text style={styles.title}>{movie.title}</Text>
-        <Text style={styles.year}>{movie.year}</Text>
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
-      </View>
-
     );
   }
 }
@@ -49,8 +99,8 @@ const styles = StyleSheet.create({
     height: 81,
   },
   rightContainer: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
+    flex: 1
+    // backgroundColor: '#FFFFFF',
   },
   title: {
     fontSize: 20,
@@ -59,7 +109,11 @@ const styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
-  }
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
